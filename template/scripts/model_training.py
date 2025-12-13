ebff2e6737b797943cc7867ba0f488a289031e1b
caecilieaj <cjep@itu.dk>
2025-12-13 13:52:26 +0100

    chore: removed redundant variable

--- template/scripts/model_training.py
+++ template/scripts/model_training.py
@@ -15,7 +15,6 @@ from sklearn.metrics import classification_report
 
 # Saving constant variables
 current_date = datetime.datetime.now().strftime("%Y_%B_%d")
-data_gold_path = "./artifacts/train_data_gold.csv" 
 data_version = "00000"
 experiment_name = current_date
 
